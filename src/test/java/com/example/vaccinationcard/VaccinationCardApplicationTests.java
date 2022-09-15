c4d401f59848aa642d814eac7f3ec4f64c98e1e0
Kim Kokubun <kim.kokubun@gmail.com>
2022-09-15 18:26:24 -0300

    first commit

---
+++ src/test/java/com/example/vaccinationcard/VaccinationCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccinationcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
