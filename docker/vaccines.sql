186aae672d8cca9fe9283e4aaf607d6d6d8da3a3
Kim Kokubun <kim.kokubun@gmail.com>
2022-09-19 18:13:10 -0300

    Idea for crosstab, need to improve

--- docker/vaccines.sql
+++ docker/vaccines.sql
@@ -32,3 +32,11 @@ insert into vaccine(name, age_in_days, mandatory, booster_in_days)
 values('Polio',60,true,ARRAY[120,182,456]);
 insert into vaccine(name, age_in_days, mandatory, booster_in_days,repeat_booster_in_days)
 values('Yellow Fever',182,true,ARRAY[3650],3650);
+
+create view crosstab_vaccines_documents as
+select *
+from crosstab('select vd.document_id,v.name,vd.document_id is not null as value from vaccine v left join vaccines_document vd on v.id = vd.vaccine_id order by 1,2') as ct ("document_id" integer,
+                                                                                                                                                                            "Tuberculosis" boolean,
+                                                                                                                                                                            "Hepatitis" boolean,
+                                                                                                                                                                            "Yellow Fever" boolean, "Polio" boolean,
+                                                                                                                                                                            "DTaP" boolean)
